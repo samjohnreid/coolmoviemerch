f0f75f8d6ca58616cced2fa7e788573bc0f8139c
Samuel Reid <sam@samuels-imac.lan>
2020-10-19 18:59:05 -0400

    adding hero products

--- data/heroes.js
+++ data/heroes.js
@@ -1,6 +1,26 @@
 export default [
   {
     id: 1,
+    title: `Funko Back to The Future Back in Time Board Game`,
+    price: 27.23,
+    url: 'https://amzn.to/3lz6hRi',
+    movie: 18,
+    category: 2,
+    license: 9,
+    img: 'funko-back-to-the-future-back-in-time-board-game'
+  },
+  {
+    id: 2,
+    title: `Jurassic Park Mosquito in Amber Resin Official Prop Replica Paper Weight`,
+    price: 27.99,
+    url: 'https://amzn.to/33Nyuhb',
+    movie: 8,
+    category: 10,
+    license: 4,
+    img: 'jurassic-park-mosquito-in-amber-resin-official-prop-replica-paper-weight'
+  },
+  {
+    id: 3,
     title: `Star Wars Darth Vader Apron, Adjustable Adult Size Fits Most`,
     price: 29.99,
     url: 'https://amzn.to/35fnKaY',
@@ -10,7 +30,7 @@ export default [
     img: 'star-wars-darth-vader-apron-adjustable-adult-size-fits-most'
   },
   {
-    id: 2,
+    id: 4,
     title: `Star Wars Rebel Officially Licensed Silicone Smartwatch Band Compatible with Apple Watch`,
     price: 39.90,
     url: 'https://amzn.to/3lAcZqf',
@@ -20,7 +40,7 @@ export default [
     img: 'star-wars-rebel-officially-licensed-silicone-smartwatch-band-compatible-with-apple-watch'
   },
   {
-    id: 3,
+    id: 5,
     title: `Fifth Element Cosplay Futuristic Strappy Stretchy Costume Bodysuit with Cutouts`,
     price: 35.27,
     url: 'https://amzn.to/3jSHLdN',
@@ -28,26 +48,6 @@ export default [
     category: 12,
     license: null,
     img: 'fifth-element-cosplay-futuristic-strappy-stretchy-costume-bodysuit-with-cutouts'
-  },
-  {
-    id: 1,
-    title: 'Shredder Cheese Grater, TMNT Teenage Mutant Ninja Turtles Official Handheld Stainless-Steel Cheese Grater',
-    price: 12.90,
-    url: 'https://amzn.to/34XHpMG',
-    movie: 1,
-    category: 1,
-    license: 19,
-    img: 'shredder-cheese-grater-tmnt-teenage-mutant-ninja-turtles-official-handheld-stainless-steel-cheese-crater'
-  },
-  {
-    id: 1,
-    title: 'Shredder Cheese Grater, TMNT Teenage Mutant Ninja Turtles Official Handheld Stainless-Steel Cheese Grater',
-    price: 12.90,
-    url: 'https://amzn.to/34XHpMG',
-    movie: 1,
-    category: 1,
-    license: 19,
-    img: 'shredder-cheese-grater-tmnt-teenage-mutant-ninja-turtles-official-handheld-stainless-steel-cheese-crater'
   },
   {
     id: 6,
@@ -58,5 +58,5 @@ export default [
     category: 1,
     license: 19,
     img: 'shredder-cheese-grater-tmnt-teenage-mutant-ninja-turtles-official-handheld-stainless-steel-cheese-crater'
-  },
+  }
 ];
