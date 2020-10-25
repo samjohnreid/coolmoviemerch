ea3ea9713f2dc06c82239a4c4fbf8f9914bba990
Samuel Reid <sam@samuels-imac.lan>
2020-10-25 17:51:39 -0400

    random movie quote search placeholder

--- data/quotes.js
+++ data/quotes.js
@@ -1,12 +1,9 @@
 export default [
-  'movie quote one',
-  'movie quote two',
-  'movie quote three',
-  'movie quote four',
-  'movie quote five',
-  'movie quote six',
-  'movie quote seven',
-  'movie quote eight',
-  'movie quote nine',
-  'movie quote ten'
+  `I'm funny how, I mean funny like I'm a clown, I amuse you?`,
+  `You talkin' to me? Well I'm the only one here`,
+  `If only you could see what I've seen with your eyes`,
+  `These aren't the droids you're looking for`,
+  `Did you ever dance with the devil in the pale moonlight?`,
+  `You're gonna need a bigger boat`,
+  `Hold on to your butts`
 ];
--- js/scripts.js
+++ js/scripts.js
@@ -32,10 +32,17 @@ document.addEventListener('click', (event) => {
 // Movie quotes for search placeholder
 
 const searchBar = document.querySelector('.masthead__search');
+const searchField = searchBar.querySelector('input');
 
+// Set movie quote randomly from array of movie quotes
 const movieQuotes = quotes;
-
 searchBar.dataset.movieQuotePlaceholder = movieQuotes[Math.floor(Math.random() * movieQuotes.length)];
+
+// Grab the current quote so we can hide it when search is being used, and restore when empty again
+const currentQuote = searchBar.dataset.movieQuotePlaceholder;
+searchField.addEventListener('blur', (e) => {
+  searchBar.dataset.movieQuotePlaceholder = e.target.value.length > 0 ? '' : currentQuote;
+});
 
 
 // ------------------------------------------------------------
