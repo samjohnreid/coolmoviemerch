537633b268865e67250e05004f89bd5ff0877e1c
Samuel Reid <sreid2@samuels-mbp.lan>
2020-11-05 01:50:07 -0500

    monkey islanding

--- js/scripts.js
+++ js/scripts.js
@@ -543,7 +543,7 @@ if (aux === 'true') {
   const auxContentAbout = `
     <p>Cool Movie Merch exists as a resource to find cool memorabilia, gadgets, toys, games, posters, accessories, apparel, office equipment, cosplay gear, face masks and any kind of merchandise related to movies... mining the web for movie gold!</p>
     <p>Most of the items you see on here will generate commission as affiliate links (the price won't be increased at all for you, it just means Cool Movie Merch gets a small cut from the seller for sending the referral).</p>
-    <p>However! This site exists primarily as a resource for ride or die movie buffs, and Cool Movie Merch is of the same ilk as you; so you will still see links to cool merch even if they don't generate any affiliate revenue&mdash;for example, these awesome <a href="https://www.soulrevolver.com/mens-leather-jackets/mens-replica-leather-jackets/fight-club">Fight Club leather jackets</a>!</p>
+    <p>However! This site exists primarily as a resource for ride or die movie buffs, and Cool Movie Merch is of the same ilk as you; so you will still see links to cool merch even if they don't generate any affiliate revenue&mdash;for example, these fine <a href="https://www.soulrevolver.com/mens-leather-jackets/mens-replica-leather-jackets/fight-club">Fight Club leather jackets</a>!</p>
     <p>That's about it, really. If you need to know more red tape stuff, then please visit the <a href="/privacy/">privacy</a>, <a href="/terms/">terms</a> or <a href="/contact/">contact</a> pages; otherwise, <a href="/">get browsing</a>!</p>
   `;
 
