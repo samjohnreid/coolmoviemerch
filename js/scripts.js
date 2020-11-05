e6de6dfe1c7b89fa0f789f6df75fb972782f6660
Samuel Reid <sreid2@samuels-mbp.lan>
2020-11-05 01:40:10 -0500

    add about content

--- js/scripts.js
+++ js/scripts.js
@@ -539,10 +539,12 @@ if (aux === 'true') {
 
   let auxContent;
 
+  // TODO: add fight club leather jacket(s) to grid!
   const auxContentAbout = `
-    <p>sadasdasdd</p>
-    <h3>about content goes here</h3>
-    <p>blah blah blah etc</p>
+    <p>Cool Movie Merch exists as a resource to find cool memorabilia, gadgets, toys, games, posters, accessories, apparel, office equipment, cosplay gear, face masks and any kind of merchandise related to movies... mining the web for movie gold!</p>
+    <p>Most of the items you see on here will generate commission as affiliate links (the price won't be increased at all for you, it just means Cool Movie Merch gets a small cut from the seller for sending the referral).</p>
+    <p>However! This site exists primarily as a resource for ride or die movie buffs, and Cool Movie Merch is of the same ilk as you; so you will still see links to cool merch even if they don't generate any affiliate revenue&mdash;for example, these awesome <a href="https://www.soulrevolver.com/mens-leather-jackets/mens-replica-leather-jackets/fight-club">Fight Club leather jackets</a>!</p>
+    <p>That's about it, really. If you need to know more red tape stuff, then please visit the <a href="/privacy/">privacy</a>, <a href="/terms/">terms</a> or <a href="/contact/">contact</a> page; otherwise, <a href="/">get browsing</a>!</p>
   `;
 
   // TODO: update this policy when i start using localStorage for favorite items, and/or if i store search terms
