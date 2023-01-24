1f5746fd73df087e7580b0fa653700ba04e43628
Samuel Reid <samjohnreid@gmail.com>
2023-01-24 16:45:52 -0500

    making new version to eventually build with React

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,1 +1,17 @@
-console.log('hi sam!');
+let PROJECT_ID = "nkto1d41";
+let DATASET = "production";
+let QUERY = encodeURIComponent('*[_type == "movie"]');
+
+let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+const directors = fetch(URL)
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    console.log('Data! Weeeeee!', data);
+  })
+  .catch((err) => {
+    console.error('Oh no!', err);
+  })
+;
